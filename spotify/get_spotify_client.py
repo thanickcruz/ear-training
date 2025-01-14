6b201e95da7306c9234c9e953d7f7e956098be64
Nick Cruz <nick@josephs-mbp-7.mynetworksettings.com>
2025-01-13 23:13:37 -0500

    Change spotify auth handling

--- spotify/get_spotify_client.py
+++ spotify/get_spotify_client.py
@@ -2,11 +2,14 @@ import yaml
 import spotipy
 from spotipy import util
 from spotipy.oauth2 import SpotifyOAuth
+import os
 
 def spotify_oauth():
     '''
     Uses hidden client credentials object to get OAuth object
     '''
+
+    """ ALTERNATIVE YAML CASE
     with open('spotify/creds.yaml') as f:
     # Load the .yaml data
         info = yaml.load(f, Loader=yaml.FullLoader)
@@ -23,3 +26,24 @@ def spotify_oauth():
         print('Connected to Spotify app!')
         
         return sp_oauth
+    """
+
+    # Fetch credentials from environment variables
+    client_id = os.getenv('SPOTIFY_CLIENT_ID')
+    client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+    redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
+
+    if not client_id or not client_secret or not redirect_uri:
+        raise ValueError("Spotify credentials are not set in environment variables.")
+
+    # Spotify OAuth setup
+    sp_oauth = SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope="user-library-read playlist-read-private playlist-read-collaborative",
+        username='cruzn0215'
+    )
+
+    print('Connected to Spotify app!')
+    return sp_oauth
